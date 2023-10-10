54acaaba8760f390ab71556597d0836459651656
Victor <kalchenkopgy@gmail.com>
2023-10-10 19:21:06 +0500

    2

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,79 +1,88 @@
-import './Board.css'
-import { useAppContext }from '../../contexts/Context'
+import "./Board.css";
+import { useAppContext } from "../../contexts/Context";
+import io from "socket.io-client";
+import Ranks from "./bits/Ranks";
+import Files from "./bits/Files";
+import Pieces from "../Pieces/Pieces";
+import PromotionBox from "../Popup/PromotionBox/PromotionBox";
+import Popup from "../Popup/Popup";
+import GameEnds from "../Popup/GameEnds/GameEnds";
 
-import Ranks from './bits/Ranks'
-import Files from './bits/Files'
-import Pieces from '../Pieces/Pieces'
-import PromotionBox from '../Popup/PromotionBox/PromotionBox'
-import Popup from '../Popup/Popup'
-import GameEnds from '../Popup/GameEnds/GameEnds'
+import arbiter from "../../arbiter/arbiter";
+import { getKingPosition } from "../../arbiter/getMoves";
+import { useEffect } from "react";
 
-import arbiter from '../../arbiter/arbiter'
-import { getKingPosition } from '../../arbiter/getMoves'
+// const socket = io.connect("http://localhost:5000");
 
 const Board = () => {
-    const ranks = Array(8).fill().map((x,i) => 8-i)
-    const files = Array(8).fill().map((x,i) => i+1)
+  const ranks = Array(8)
+    .fill()
+    .map((x, i) => 8 - i);
+  const files = Array(8)
+    .fill()
+    .map((x, i) => i + 1);
 
-    const { appState } = useAppContext();
-    const position = appState.position[appState.position.length - 1]
+  const { appState } = useAppContext();
+  const position = appState.position[appState.position.length - 1];
+  //   console.log(position);
 
-    const checkTile = (() => {
-        const isInCheck =  (arbiter.isPlayerInCheck({
-            positionAfterMove : position,
-            player : appState.turn
-        }))
+  //   useEffect(() => {
+  //     console.log(position[0][0]);
+  //     socket.emit("changeBoard", (socket, JSON.stringify(position)));
+  //   }, []);
+  const checkTile = (() => {
+    const isInCheck = arbiter.isPlayerInCheck({
+      positionAfterMove: position,
+      player: appState.turn,
+    });
 
-        if (isInCheck)
-            return getKingPosition (position, appState.turn)
+    if (isInCheck) return getKingPosition(position, appState.turn);
 
-        return null
-    })()
+    return null;
+  })();
 
-    const getClassName = (i,j) => {
-        let c = 'tile'
-        c+= (i+j)%2 === 0 ? ' tile--dark ' : ' tile--light '
-        if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)){
-            if (position[i][j])
-                c+= ' attacking'
-            else 
-                c+= ' highlight'
-        }
-
-        if (checkTile && checkTile[0] === i && checkTile[1] === j) {
-            c+= ' checked'
-        }
-
-        return c
+  const getClassName = (i, j) => {
+    let c = "tile";
+    c += (i + j) % 2 === 0 ? " tile--dark " : " tile--light ";
+    if (appState.candidateMoves?.find((m) => m[0] === i && m[1] === j)) {
+      if (position[i][j]) c += " attacking";
+      else c += " highlight";
     }
 
-    return <div className='board'>
+    if (checkTile && checkTile[0] === i && checkTile[1] === j) {
+      c += " checked";
+    }
 
-        <Ranks ranks={ranks}/>
+    return c;
+  };
 
-        <div className='tiles'>
-            {ranks.map((rank,i) => 
-                files.map((file,j) => 
-                    <div 
-                        key={file+''+rank} 
-                        i={i}
-                        j={j}
-                        className={`${getClassName(7-i,j)}`}>
-                    </div>
-                ))}
-        </div>
+  return (
+    <div className="board">
+      <Ranks ranks={ranks} />
 
-        <Pieces/>
+      <div className="tiles">
+        {ranks.map((rank, i) =>
+          files.map((file, j) => (
+            <div
+              key={file + "" + rank}
+              i={i}
+              j={j}
+              className={`${getClassName(7 - i, j)}`}
+            ></div>
+          ))
+        )}
+      </div>
 
-        <Popup>
-            <PromotionBox />
-            <GameEnds />
-        </Popup>
+      <Pieces />
 
-        <Files files={files}/>
+      <Popup>
+        <PromotionBox />
+        <GameEnds />
+      </Popup>
 
+      <Files files={files} />
     </div>
-    
-}
+  );
+};
 
-export default Board
+export default Board;
--- src/components/Control/bits/TakeBack.js
+++ src/components/Control/bits/TakeBack.js
@@ -1,13 +1,14 @@
-import { useAppContext }from '../../../contexts/Context'
-import { takeBack } from '../../../reducer/actions/move';
+import { useAppContext } from "../../../contexts/Context";
+import { takeBack } from "../../../reducer/actions/move";
 
 const TakeBack = () => {
+  const { dispatch } = useAppContext();
 
-    const { dispatch } = useAppContext();
-
-    return <div>
-        <button onClick={() => dispatch(takeBack())}>Take Back</button>
+  return (
+    <div>
+      {/* <button onClick={() => dispatch(takeBack())}>Take Back</button> */}
     </div>
-}
+  );
+};
 
-export default TakeBack
+export default TakeBack;
